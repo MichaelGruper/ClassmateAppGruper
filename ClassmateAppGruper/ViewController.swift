c1b95e49c3078970698f87a55e0fbe9a996df220
Michael Gruper <sarge43210@gmail.com>
2024-10-02 07:26:33 -0500

    Initial Commit

---
+++ ClassmateAppGruper/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  ClassmateAppGruper
+//
+//  Created by MICHAEL GRUPER on 10/2/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ ClassmateAppGruperTests/ClassmateAppGruperTests.swift
@@ -0,0 +1,17 @@
+//
+//  ClassmateAppGruperTests.swift
+//  ClassmateAppGruperTests
+//
+//  Created by MICHAEL GRUPER on 10/2/24.
+//
+
+import Testing
+@testable import ClassmateAppGruper
+
+struct ClassmateAppGruperTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
